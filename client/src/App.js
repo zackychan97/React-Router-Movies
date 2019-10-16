5d42da73b7691aa062ad1086891190d9a743ae31
Zack Bumpous <upwork.ztb@gmail.com>
2019-10-16 11:08:13 -0500

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,12 @@
-// import React, { useState } from 'react';
-// import MovieList from './Movies/MovieList';
-// import SavedList from './Movies/SavedList';
-// import {Route} from 'react-router-dom';
-// import Movie from './Movies/Movie';
-
-
-// const App = () => {
-//   const [savedList, setSavedList] = useState( [] );
-
-//   const addToSavedList = movie => {
-//     setSavedList( [...savedList, movie] );
-//   };
-
-//   return (
-//     <div>
-//       <SavedList list={savedList} />
-//       <Route exact path ='/' render={()=> <MovieList />} />
-//       <Route path ='/movies/:id' render={props => <Movie {...props} addToSavedList={addToSavedList} />} />
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
 
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
-
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
 
 const App = () => {
-  const [savedList, setSavedList] = useState([]);
-
+  const [savedList, setSavedList] = useState([]);                  
   const addToSavedList = movie => {
     const present = savedList.find(item => item.title === movie.title);
     if (!present) {
